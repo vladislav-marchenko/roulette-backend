79196eeda71ba3da4700a46d54e0d817b61b5b0b
Владислав <maksimsraud@gmail.com>
2025-08-26 15:31:03 +0300

    fix: handled user entity not found error

--- src/gramjs/gramjs.service.ts
+++ src/gramjs/gramjs.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common'
+import {
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+  BadRequestException,
+} from '@nestjs/common'
 import { TelegramClient } from 'telegram'
 import { StringSession } from 'telegram/sessions'
 import { Api } from 'telegram'
@@ -58,19 +63,28 @@ export class GramjsService implements OnModuleInit, OnModuleDestroy {
     username: string
     giftId: string
   }) {
+    const errorMessage =
+      'Unable to send gift: user not found. Please set a username in Telegram or simply send any message to @giftica_support so we can access your ID.'
+
     let user: Entity
+    try {
+      if (username) {
+        user = await this.client.getEntity(username)
+      } else {
+        user = await this.client.getEntity(telegramId)
+      }
+    } catch (error) {
+      if (
+        error instanceof Error &&
+        error.message.includes('Could not find the input entity')
+      ) {
+        throw new BadRequestException(errorMessage)
+      }
 
-    if (username) {
-      user = await this.client.getEntity(username)
-    } else {
-      user = await this.client.getEntity(telegramId)
+      throw error
     }
 
-    if (!(user instanceof Api.User)) {
-      throw new Error(
-        'Unable to send gift: user not found. Please set a username in Telegram or simply send any message to @giftica_support so we can access your ID.',
-      )
-    }
+    if (!(user instanceof Api.User)) throw new BadRequestException(errorMessage)
 
     const invoice = new Api.InputInvoiceStarGift({
       peer: new Api.InputPeerUser({
